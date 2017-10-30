127c08f7fd04144a8a4adf45b31ddd2953c50a3f
tanuj.garg <tanuj.garg@students.iiit.ac.in>
2017-10-30 18:32:00 +0530

    q6 edited

--- q6.sh
+++ q6.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 read number
 echo -n "("
-# replacing all brackets with blank spaces and then printing a pair of brackets at extremes
-a=`echo $number | tr "(" " " | tr ")" " "`
+# replacing all brackets with blank spaces and then printing a pair of brackets at extremesUa=`echo $number | tr "(" " " | tr ")" " " | `
+a=`echo $number | tr "(" " " | tr ")" " " | tr -s " "`
 echo -n $a
 echo -n ")"
 echo
